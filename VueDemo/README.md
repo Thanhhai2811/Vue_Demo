b892dc88961827ec73827330a2ea1c61604b3c23
Trần Hải <tranthanhhai200000@gmail.com>
2024-07-16 13:51:58 +0700

    Delete unnecessary files

---
+++ VueDemo/README.md
@@ -0,0 +1,9 @@
+1. Component Concept
+    - Structure in component
+        + props : Cho chép truyền dữ liệu từ bên ngoài vào bên  trong component, ý nghĩa của việc viết props là
+        sẽ thay đổi yêu cầu mong muốn, so với code base ban đầu
+        
+        + emits : cho phép truyền dữ liệu từ thằng con về thằng cha ,bắt sự kiện thông qua event
+        + watch : 
+        + computed : bản chất là 1 attribute, cho phép xử lý logic bên trong, dùng nó khi muốn biến đổi một dữ liệu
+                     có sẵn sang một dữ liệu mới 
---
+++ VueDemo/src/components/VueDay.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h3>{{ labelName }}</h3>
+      <input type="file" name="file" @change="chooseFile"/>
+      <br/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps, defineEmits} from 'vue'
+
+const props = defineProps ({
+  labelName : {
+    type : String,
+    default: 'Chọn file'
+  }
+})
+
+const emits = defineEmits(['choose-file']) // (choose-file) là giá trị được truyền 
+
+
+const chooseFile = (event: any) => {
+  // gửi thông tin đến component cha 
+  
+  let fileInfo = event.target.files[0]
+  emits('choose-file', fileInfo)
+}
+</script>
+
+<style>
+
+</style>
